99cf1cb88a283a2ca0f51d4e8edef5339893c9c5
MartinMadisson <martinmadisson2008@gmail.com>
2026-02-20 12:39:49 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,23 +1,23 @@
-async function getData() { //see ootab andmeid internetist
+async function getData() { 
   const kass = document.querySelector("#kass");
   const response = await fetch('https://api.thecatapi.com/v1/images/search'); //ikka ootab ja on ootel API lehekulg aitab teha
   const data = await response.json(); //fetch saab internetist andmed ja see on ootel, siis saab JSON vormingus andmed kätte
   const object = data[0]; //Kui andmed kaes olid olid andmed arrays ja siis et saada koike sealt katte panin index 0
   const imageUrl = object.url; //vottis URL
-  console.log(data[0]); //sai vaatada kas tootas see array data ja index 0 oli olemas
+  console.log(data[0]); 
 
-  if (document.querySelector(".Kassipilt")) { //kui on olemas kassipilt siis see eemaldab viimase pildi ja tuleb uus pilt
+  if (document.querySelector(".Kassipilt")) { 
     document.querySelector(".Kassipilt").remove();
   }
 
-  const image = document.createElement("img"); //teeb uue pildi
-  image.className = "Kassipilt"; //panin classinimeks kassipilt siis on arusaadav ja et parast peab ules leidma ja ara kustutada 1 rida uleval
-  image.src = imageUrl; //pilt tuleb siit
+  const image = document.createElement("img"); 
+  image.className = "Kassipilt";
+  image.src = imageUrl;
   kass.appendChild(image);
 
 }
 
-const nupp = document.querySelector("#nupp"); //kui vajutada tuleb uus pilt
-nupp.addEventListener("click", getData); //tegin kui kasutaja peab midagi tegema siis tuleks uus pilt ehk otsast peale ulevalt alla
+const nupp = document.querySelector("#nupp"); 
+nupp.addEventListener("click", getData);
 
 getData();
